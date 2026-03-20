97c5bee9123edb159584228c51d0ef10c31db8d1
Gustavo Cavalcante <gustavocavalcante@MacBook-Air-de-Gustavo.local>
2026-03-20 06:48:17 -0400

    feat: integrate electron-updater in main process

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,44 @@
 import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { autoUpdater } from 'electron-updater'
 import { runMigrations } from './db/migrate'
 import { seedIfEmpty } from './db/seed'
 import { registerAllHandlers } from './handlers'
+import { IPC } from '../shared/ipc-channels'
+
+let mainWindow: BrowserWindow | null = null
+
+function setupAutoUpdater(): void {
+  autoUpdater.autoDownload = true
+  autoUpdater.autoInstallOnAppQuit = true
+
+  // In dev, don't check for updates
+  if (is.dev) return
+
+  autoUpdater.on('update-available', (info) => {
+    mainWindow?.webContents.send(IPC.UPDATE_AVAILABLE, { version: info.version })
+  })
+
+  autoUpdater.on('download-progress', (progress) => {
+    mainWindow?.webContents.send(IPC.UPDATE_PROGRESS, { percent: Math.round(progress.percent) })
+  })
+
+  autoUpdater.on('update-downloaded', (info) => {
+    mainWindow?.webContents.send(IPC.UPDATE_DOWNLOADED, { version: info.version })
+  })
+
+  autoUpdater.on('error', (err) => {
+    mainWindow?.webContents.send(IPC.UPDATE_ERROR, { message: err.message })
+  })
+
+  // Check on startup, then every 4 hours
+  autoUpdater.checkForUpdates().catch(() => {})
+  setInterval(() => autoUpdater.checkForUpdates().catch(() => {}), 4 * 60 * 60 * 1000)
+}
 
 function createWindow(): void {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
     show: false,
@@ -17,19 +49,19 @@ function createWindow(): void {
     },
   })
 
-  win.on('ready-to-show', () => {
-    win.show()
+  mainWindow.on('ready-to-show', () => {
+    mainWindow!.show()
   })
 
-  win.webContents.setWindowOpenHandler((details) => {
+  mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
 
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    win.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
-    win.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
 }
 
@@ -45,6 +77,7 @@ app.whenReady().then(() => {
   runMigrations()
   seedIfEmpty()
   createWindow()
+  setupAutoUpdater()
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
